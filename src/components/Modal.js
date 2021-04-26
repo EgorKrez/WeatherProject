fb598ef69ed24dbf62dd2635094e2ff597b01f25
Yahor Krez <Yahor.Krez@itechart-group.com>
2021-04-26 15:10:04 +0300

    App has been uppgraded

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -26,24 +26,14 @@ const Modal = ({
     deg = forecast.cityWind.deg;
   };
 
-  const getItemClass = (forecast) => {
-    return forecast.weather === "Clear"
-      ? "modal-menu-item sunny"
-      : forecast.weather === "Clouds"
-      ? "modal-menu-item cloud"
-      : forecast.weather === "Rain"
-      ? "modal-menu-item rain"
-      : "modal-menu-item";
-  };
-
-  const getModalClass = (forecast) => {
-    return forecast === "Clear"
-      ? "modal-item sunny"
-      : forecast === "Clouds"
-      ? "modal-item cloud"
-      : forecast === "Rain"
-      ? "modal-item rain"
-      : "modal-item";
+  const getClass = (weather, itemClass) => {
+    return weather === "Clear"
+      ? `${itemClass} sunny`
+      : weather === "Clouds"
+      ? `${itemClass} cloud`
+      : weather === "Rain"
+      ? `${itemClass} rain`
+      : `${itemClass}`;
   };
 
   return (
@@ -52,7 +42,7 @@ const Modal = ({
       onClick={() => setActive(false)}
     >
       <div
-        className={getModalClass(weather)}
+        className={getClass(weather, "modal-item")}
         onClick={(e) => e.stopPropagation()}
       >
         <div className="item-time">{time}</div>
@@ -65,53 +55,20 @@ const Modal = ({
         </div>
         {forecast ? (
           <div className="modal-menu">
+            {forecast.map((item) => (
+              <div
+                key={item.time}
+                className={getClass(item.weather, "modal-menu-item")}
+                onClick={() => showForecast(item)}
+              >
+                {item.time}
+              </div>
+            ))}
             <div
-              className={getItemClass(forecast[0])}
-              onClick={() => showForecast(forecast[0])}
-            >
-              {JSON.stringify(forecast[0].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[1])}
-              onClick={() => showForecast(forecast[1])}
-            >
-              {JSON.stringify(forecast[1].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[2])}
-              onClick={() => showForecast(forecast[2])}
-            >
-              {JSON.stringify(forecast[2].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[3])}
-              onClick={() => showForecast(forecast[3])}
-            >
-              {JSON.stringify(forecast[3].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[4])}
-              onClick={() => showForecast(forecast[4])}
-            >
-              {JSON.stringify(forecast[4].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[5])}
-              onClick={() => showForecast(forecast[5])}
-            >
-              {JSON.stringify(forecast[5].time)}
-            </div>
-            <div
-              className={getItemClass(forecast[6])}
-              onClick={() => showForecast(forecast[6])}
-            >
-              {JSON.stringify(forecast[6].time)}
-            </div>
-            <div
-              className={getItemClass(currentItem)}
+              className={getClass(currentItem.weather, "modal-menu-item")}
               onClick={() => showForecast(currentItem)}
             >
-              "{currentItem.time}"
+              {currentItem.time}
             </div>
           </div>
         ) : (
